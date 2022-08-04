f79c1d186965a8572b1599bdd51e74805218faf8
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-08-04 17:51:01 -0300

    feat: create productOrders table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from .products_model import Product
 from .orders_model import Order
 from .clients_model import Client
 from .client_orders_model import ClientOrder
+from .product_orders_model import ProductOrder

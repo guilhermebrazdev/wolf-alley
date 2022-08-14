f3100d4a95d9ec639d012839c992f4ac2cb02416
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-08-14 16:42:03 -0300

    feat: create sold orders model

---
+++ app/models/sold_orders_model.py
@@ -0,0 +1,15 @@
+from app.configs.database import db 
+from sqlalchemy import Column, Integer, ForeignKey
+from dataclasses import dataclass
+
+@dataclass
+class SoldOrder(db.Model):
+    __tablename__ = "sold_orders"
+
+    id: int
+    sold_product_id: int
+    all_order_id: int
+
+    id = Column(Integer, primary_key=True)
+    sold_product_id = Column(Integer, ForeignKey('sold_products.id'))
+    all_order_id = Column(Integer, ForeignKey('all_orders.id'))

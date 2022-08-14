a926ee433e5176d3763bd9865e644f6ca25bac59
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-08-14 17:43:00 -0300

    feat: create all_orders table and migration file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,3 +5,4 @@ from .clients_model import Client
 from .client_orders_model import ClientOrder
 from .product_orders_model import ProductOrder
 from .sold_products_model import SoldProduct
+from .all_orders_model import AllOrder

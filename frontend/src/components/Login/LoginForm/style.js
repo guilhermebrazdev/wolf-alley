a35e4cad7c93aa85725e00ab25f0d6a1e2bc040e
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-09-26 23:11:44 -0300

    feat: implements form connection

--- frontend/src/components/Login/LoginForm/style.js
+++ frontend/src/components/Login/LoginForm/style.js
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: var(--darkBlue);
-  width: 300px;
-  height: 200px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  border-radius: 8px;
+  form {
+    background: var(--darkBlue);
+    width: 300px;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    border-radius: 8px;
+  }
 `;

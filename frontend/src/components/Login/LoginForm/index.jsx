5b5e962ef18934d08d5a824e98a778d6d319a96d
Guilherme Braz <guilhermebrazmoreirafaria@gmail.com>
2022-09-27 19:56:30 -0300

    feat: implements cors permission

--- frontend/src/components/Login/LoginForm/index.jsx
+++ frontend/src/components/Login/LoginForm/index.jsx
@@ -13,10 +13,8 @@ import { Container } from "./style";
 const LoginForm = () => {
   const LoginSchema = yup.object().shape({
     email: yup.string().required("Campo obrigatório").email("Email inválido"),
-    password: yup
-      .string()
-      .required("Campo obrigatório")
-      .min(6, "Mínimo de 6 dígitos"),
+    password: yup.string().required("Campo obrigatório"),
+    // .min(6, "Mínimo de 6 dígitos"),
   });
 
   const {
